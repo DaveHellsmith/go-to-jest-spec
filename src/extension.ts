46900bd96f6d5c804abaee44da5e9d771d748969
Dave Demirkhanyan <daid.demirkhanyan@vineti.com>
2017-12-02 16:43:43 +0400

    cleanup and refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,3 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as resolver from './resolver';
 import * as fs from 'fs';
@@ -25,46 +22,29 @@ function prompt(fileName, cb) {
 			});
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.jestGoToSpec', () => {
-	// The code you place here will be executed every time your command is executed
+		var editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return; 
+		}
 
-	// Display a message box to the user
-	var editor = vscode.window.activeTextEditor;
-	if (!editor) {
-		return; // No open text editor
-	}
-
-	let document = editor.document;
-	let fileName: string = document.fileName;
-	let related: string = resolver.getRelated(fileName);
-	let relative: string = vscode.workspace.asRelativePath(related);
-	let fileExists: boolean = fs.existsSync(related);
-	let dirname: string = path.dirname(related);
-	
-	//console.log('fileExists', fileExists);
-
-	if (fileExists) {
-		openFile(related);
-	} else {
-		prompt(relative, function() {
-			mkdirp.sync(dirname);
-			fs.closeSync(fs.openSync(related, 'w'));
+		let document = editor.document;
+		let fileName: string = document.fileName;
+		let related: string = resolver.switchFile(fileName);
+		let relative: string = vscode.workspace.asRelativePath(related);
+		let fileExists: boolean = fs.existsSync(related);
+		let dirname: string = path.dirname(related);
+		
+		if (fileExists) {
 			openFile(related);
-		});
-	}
-
+		} else {
+			prompt(relative, function() {
+				mkdirp.sync(dirname);
+				fs.closeSync(fs.openSync(related, 'w'));
+				openFile(related);
+			});
+		}
 	});
-
 	context.subscriptions.push(disposable);
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() {
 }
--- src/resolver.ts
+++ src/resolver.ts
@@ -1,22 +1,18 @@
-export function getRelated(file) {
-	if (isSpec(file)) {
-		return specToCode(file);
-	} else {
-		return codeToSpec(file);
-	}
+export function switchFile(file) {
+	return isSpec(file) ? specToCode(file) : codeToSpec(file);
 }
 
-export function isSpec(file) {
+function isSpec(file) {
 	return file.indexOf('.spec.js') > -1;
 }
 
-export function codeToSpec(file) {
+function codeToSpec(file) {
 	file = file.replace('.js', '.spec.js');
 	
 	return file.replace('/app/', '/spec/');
 }
 
-export function specToCode(file: string) {
+function specToCode(file: string) {
 	file = file.replace('.spec.js', '.js');
 
 	return file.replace('/spec/', '/app/');
